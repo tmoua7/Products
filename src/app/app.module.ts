c599c769b6e460993d4ae2134c22d4b4b23ded68
Thomas Moua <65568114+tmoua7@users.noreply.github.com>
2021-11-25 00:54:17 -0800

    ShippingComponent to use CartService

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,6 +11,7 @@ import { TopBarComponent } from './top-bar/top-bar.component';
 import { ProductAlertsComponent } from './product-alerts/product-alerts.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { CartComponent } from './cart/cart.component';
+import { ShippingComponent } from './shipping/shipping.component';
 
 @NgModule({
   declarations: [
@@ -18,17 +20,20 @@ import { CartComponent } from './cart/cart.component';
     TopBarComponent,
     ProductAlertsComponent,
     ProductDetailsComponent,
-    CartComponent
+    CartComponent,
+    ShippingComponent,
   ],
   imports: [
     BrowserModule,
     //AppRoutingModule
     ReactiveFormsModule,
+    HttpClientModule,
     RouterModule.forRoot([
       //the name of Navigation of each page for routerLink
       { path: '', component: ProductListComponent },
       { path: 'products/:productId', component: ProductDetailsComponent },
       { path: 'cart', component: CartComponent },
+      { path: 'shipping', component: ShippingComponent },
     ])
   ],
   providers: [],
--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,11 +1,15 @@
 import { Injectable } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 import { Product } from './products';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CartService {
+
+  constructor(
+    private http: HttpClient
+  ) { }
 
   items: Product[] = [];
   addToCart(product: Product) {
@@ -19,5 +23,9 @@ export class CartService {
   clearCart() {
     this.items = [];
     return this.items;
+  }
+
+  getShippingPrices() {
+    return this.http.get<{ type: string, price: number }[]>('/assets/shipping.json');
   }
 }
